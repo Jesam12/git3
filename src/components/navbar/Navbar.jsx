ecc1164226f3d6aab4a51600ff9bac818fe16d7b
jesam12 <jmetro181@gmail.com>
2024-05-08 03:54:07 +0100

    Added a sign up and sign in button to display when you toggle the handburger menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -43,6 +43,12 @@ const Navbar = () => {
          <div className="gpt3__navbar-menu_container-links">
             <Menu/>
           </div>
+
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+
       </div>
         )}
      </div>
