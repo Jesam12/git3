aa15103e77c8d1fdb3569da48e321dbe27b5bb0f
jesam12 <jmetro181@gmail.com>
2024-05-08 22:25:47 +0100

    Adding styles (header.css)

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='gpt3__header section__padding' id="home">
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>
-          Let's Build Something Amazing With GPT-# OpenAI
+          Let's Build Something Amazing With GPT-3 OpenAI
         </h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
