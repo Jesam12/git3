c4db37719984325db9801f3ba8abde5260a28a8b
jesam12 <jmetro181@gmail.com>
2024-05-08 22:21:12 +0100

    Finished working with Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
@@ -8,8 +10,20 @@ const Header = () => {
         <h1 className='gradient__text'>
           Let's Build Something Amazing With GPT-# OpenAI
         </h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
         <div className='gpt3__header-content__input'>
           <input type='email' placeholder='Your Email Address'/>
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people}/>
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div className='gpt3__header-image'>
+          <img src={ai} alt='ai'/>
         </div>
       </div>
     </div>
