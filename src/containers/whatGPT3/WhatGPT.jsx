f44672370d2863b66dda7c4290c5e9edb8319517
jesam12 <jmetro181@gmail.com>
2024-05-11 00:35:57 +0100

    WhatGPT.jsx commit

--- src/containers/whatGPT3/WhatGPT.jsx
+++ src/containers/whatGPT3/WhatGPT.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './whatGPT.css'
+import { Feature } from '../../components';
 
 const WhatGPT = () => {
   return (
-    <div>
-      WhatGPT
+    <div className='gpt3__whatgpt3 section__margin'>
+      <div className="gpt3__whatgpt3-feature">
+       <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
